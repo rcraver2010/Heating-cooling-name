77f858051b8bac79e576e3078d5007d17458dc7e
Rica Craver <rica.craver@yahoo.com>
2022-10-17 17:13:01 -0400

    Finished name lab

---
+++ Longest Name/name.js
@@ -0,0 +1,10 @@
+let nameOne ="Rica Craver"; //10 letters
+let nameTwo ="Jessica Harris"; //13 letters
+let nameThree ="Brandon smithjohn"; //16 letters
+
+if (nameOne.length < nameTwo.length && nameTwo.length > nameOne.length) {
+    console.log `${nameTwo} has the longest name.`;
+}
+else if(nameTwo.length > nameThree.length) {
+    console.log `${nameThree} has the longest name.`;
+}
