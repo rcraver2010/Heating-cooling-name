ed07462620ce1fd61bc8130342579bf1ee6a50c7
Rica Craver <rica.craver@yahoo.com>
2022-10-17 09:57:50 -0400

    Finished heating/cooling lab

---
+++ heating.js
@@ -0,0 +1,12 @@
+let actualTemp = 67;
+let desiredTemp = 72;
+
+if(actualTemp < desiredTemp) {
+    console.log('Run Heat');
+}
+else if (actualTemp > desiredTemp) {
+    console.log("Run A/C");
+}
+else {
+    console.log('Standby');
+}
